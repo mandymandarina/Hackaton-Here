76e25f6d3ab4b1a8af73f01c89d19e4e34ec66ee
Tammy <tamara.hi.sa@hotmail.com>
2018-10-02 19:55:11 -0300

    Se autentica registro y login con firebase

--- src/Login/login.js
+++ src/Login/login.js
@@ -1,14 +1,14 @@
-//Registro
+// Registro
 function registerWithFirebase(){
     const emailValue = email.value;
     const passwordValue = password.value;
 
     if(emailValue.length != 0 && passwordValue.length != 0) {
-        console.log('if')
+        console.log('if');
     firebase.auth().createUserWithEmailAndPassword(emailValue, passwordValue)
         .then(()=>{
             console.log("Usuario creado con éxito");
-            location.href ="menu.html";
+            location.href ="";
         })
         .catch((error)=>{
             console.log("Error de firebase > Código > "+error.code); //error.code nos mostrará el código de error para informarnos qué pasó
@@ -16,7 +16,7 @@ function registerWithFirebase(){
         });
 }};
 
-//Login
+// Login
 function loginWithFirebase(){
     const emailValue = email.value;
     const passwordValue = password.value;
@@ -26,10 +26,23 @@ function loginWithFirebase(){
     firebase.auth().signInWithEmailAndPassword(emailValue, passwordValue)
         .then(()=>{
             console.log("Usuario inició sesión con éxito");
-            location.href ="menu.html";
+            location.href ="";
         })
         .catch((error)=>{
             console.log("Error de firebase > Código > "+error.code); //error.code nos mostrará el código de error para informarnos qué pasó
             console.log("Error de firebase > Mensaje > "+error.message); //error.message nos mostrará el mensaje de firebase del mismo error
         });
 }}
+
+// Facebook Login
+function loginFacebook() {
+    const provider = new firebase.auth.FacebookAuthProvider();
+
+    auth().signInWithPopup(provider).then(function(result){
+        alert("Exito");
+        console.log(result);
+    }).catch(function(error){
+        alert("Error");
+        console.log(error);
+    });
+}
